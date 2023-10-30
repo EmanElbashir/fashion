b35592107320d2ec23c3ef03ae34c6fcab1cde73
Eman Elbashir <114422580+EmanElbashir@users.noreply.github.com>
2023-10-30 06:34:51 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+  
+  /*
+  <img data-srchover="This_1.jpg" src="main.jpg" alt="Eman One" />
+
+  const images = document.querySelectorAll('[data-srchover]');
+  images.forEach((iamge)=>{
+    image.addEventListener('mouseHover', ()=>{
+    image.src = image.dataset.src
+  })
+  })*/

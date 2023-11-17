49cb2f859dc6c860534c110ebe67a50637903bb4
Eman Elbashir <114422580+EmanElbashir@users.noreply.github.com>
2023-11-17 23:50:49 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -23,29 +23,29 @@
 
 
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const animatedImage = document.querySelector('.scrol');
-    const screenWidth = window.innerWidth;
-    const halfwayPoint = screenWidth / 2;
+  // document.addEventListener('DOMContentLoaded', function () {
+  //   const animatedImage = document.querySelector('.scrol');
+  //   const screenWidth = window.innerWidth;
+  //   const halfwayPoint = screenWidth / 2;
 
-    function animate() {
-      // Move from right to left
-      animatedImage.style.transform = 'translateX(-10%)';
+  //   function animate() {
+  //     // Move from right to left
+  //     animatedImage.style.transform = 'translateX(-10%)';
 
-      // Scale at the halfway point
-      setTimeout(() => {
-        animatedImage.style.transform = 'translateX(-50%) scale(1.5)';
-        animatedImage.style.width = '50%';
-      }, 1000); // Adjust the delay based on your preference
+  //     // Scale at the halfway point
+  //     setTimeout(() => {
+  //       animatedImage.style.transform = 'translateX(-50%) scale(1.5)';
+  //       animatedImage.style.width = '50%';
+  //     }, 1000); // Adjust the delay based on your preference
 
-      // Continue moving
-      setTimeout(() => {
-        animatedImage.style.transform = 'translateX(-100%)';
-      }, 2000); // Adjust the delay based on your preference
-    }
+  //     // Continue moving
+  //     setTimeout(() => {
+  //       animatedImage.style.transform = 'translateX(-100%)';
+  //     }, 2000); // Adjust the delay based on your preference
+  //   }
 
-    animate();
-  });
+  //   animate();
+  // });
   
   
   
